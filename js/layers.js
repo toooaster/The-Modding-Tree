6ff8d37f043363617a9b99e9ec55bf96d24d8d69
toooaster <mazemazer@1000uf.com>
2026-04-06 11:23:47 +0200

    bug fix

--- js/layers.js
+++ js/layers.js
@@ -15,7 +15,7 @@ addLayer("p", {
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
-        if (hasUpgrade('p', 12)) gain = gain.times(upgradeEffect('p', 12))
+        if (hasUpgrade('p', 12)) mult = mult.times(upgradeEffect('p', 12))
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
